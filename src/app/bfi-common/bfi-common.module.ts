63675d2bcd52a89f1b78a47d3c9944c2bece6770
Tommy <rabesalama.tommy@gmail.com>
2022-12-11 16:13:11 +0300

    Create a navbar component within a bfi-common Angular module

---
+++ src/app/bfi-common/bfi-common.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BfiNavbarComponent } from './bfi-navbar/bfi-navbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    BfiNavbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BfiCommonModule { }
